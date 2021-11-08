720e31c8fe22bf998ce915f9d0dda51af733b0cc
JandersonLRibeiro <jandersonlucasribeiro@gmail.com>
2021-11-08 15:04:45 -0300

    Teste

--- visao_01.py
+++ visao_01.py
@@ -1,1 +1,7 @@
-print('Hello Word')
+num1 = input("Digite o numero [01]: ")
+num2 = input("Digite o numero [02]: ")
+num3 = input("Digite o numero [03]: ")
+
+if(num1 > num2 and num1 > num3 and num2 > num3):
+    print("(f'A ordem decrescente é {num1} , {num2} e {num3}')")
+

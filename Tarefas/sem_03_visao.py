a208d9feee56859fc403333e2b0c656041cc8577
JandersonLRibeiro <jandersonlucasribeiro@gmail.com>
2021-11-24 10:23:26 -0300

    .

---
+++ Tarefas/sem_03_visao.py
@@ -0,0 +1,5 @@
+from tf.keras.models import Sequential
+from tf.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tf.keras.layers.normalization import BatchNormalization 
+from tf.keras.preprocessing.image import ImageDataGenerator
+
